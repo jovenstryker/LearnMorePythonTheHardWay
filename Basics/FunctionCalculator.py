02b68f5a8caaa280f98e7d03f7d389305968f0e4
Joven Sagrado <joven.sagrado@stryker.com>
2022-02-22 14:08:24 +0000

    Added Basics Folder with files

---
+++ Basics/FunctionCalculator.py
@@ -0,0 +1,15 @@
+def add_numbers(num1, num2):
+    print(num1 + num2)
+
+
+def sub_numbers(num1, num2):
+    print(num1 - num2)
+
+
+def div_numbers(num1, num2):
+    print(num1 / num2)
+
+
+add_numbers(1, 2)
+sub_numbers(3, 2)
+div_numbers(3, 3)
---
+++ Basics/MadLibsGame.py
@@ -0,0 +1,7 @@
+hero = ["John", "Camelot", "Fedor", "Kylo", "Shen", "Vlad"]
+mount = ["Lir", "Stew", "Gnar", "Exols", "Neis", "Minty"]
+villain = ["Drakeus", "Impetour", "Essalseth", "Amoundrath", "Baroeg", "Velmour"]
+location = ["Hean", "Leas", "Ouirz", "Qart", "Raeus", "Torken"]
+
+for i in range(6):
+    print("The hero ", str(hero[i]), "rode his mount ", str(mount[i]), "to fight ", str(villain[i]), "in ", location[i])
